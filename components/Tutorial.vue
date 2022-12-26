7d66142e5ea2e035f235d66aa418255e2114d1ec
shadiqsharma <karryjane@asenford.tech>
2022-12-26 19:29:56 +0800

    new push

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,12 +1,15 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0 bg-yellow-200" >
+  <div class="relative flex items-top justify-center min-h-screen bg-cover bg-smbackdrop  sm:pt-0 " >
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 ">
+
       <a class="flex justify-center pt-8 sm:pt-0" href="https://nuxtjs.org" target="_blank">
-        <img src="~assets/img/Logo.png" />
+        <img src="~assets/img/landingimage_mobile.png" />
       </a>
-      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6 text-center">
-        LA BO-TAGGED
+
+      <div class="mt-10 bg-red-900 overflow-hidden shadow sm:rounded-lg p-6 text-center full-width">
+        <h1 class="text-center text-white text-4xl pb-2.5">Come Collect your Prize!</h1>
+        <div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=La%20Bodega%20Langkawi,%2007000%20Langkawi,%20Kedah+(labodega%20langkawi)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure acres/hectares on map</a></iframe></div>
       </div>
 
     </div>
---
+++ pages/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-cover bg-smbackdrop sm:items-center sm:pt-0 " >
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 ">
+      <a class="flex justify-center pt-8 sm:pt-0" href="https://nuxtjs.org" target="_blank">
+        <img src="~assets/img/landingimage_mobile.png" />
+      </a>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6 text-center">
+        LA BO-TAGGED
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
